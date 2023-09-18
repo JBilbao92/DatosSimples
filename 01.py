056d321cf329c685b2e2be8b779838b413bf480b
Javier Bilbao <145110561+JBilbao92@users.noreply.github.com>
2023-09-18 09:01:47 +0200

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,16 @@
+'''##En este archivo vienen todos los ejercicios, los ejercicios resueltos se dejaran como comentario
+
+Ejercicio 1
+print("¡Hola Mundo!")
+
+Ejercicio 2
+texto = "¡Hola Mundo!"
+print(texto)
+'''
+Ejercicio 3
+nombre = input("Introduce tu nombre: ")
+print("¡Hola " + nombre + "!")
+
+##Ejercicio 4
+
+
