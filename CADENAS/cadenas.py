335e4e0ec5574748999411cc599638b85362a52a
Javier.Bilbao <jbilbao92@gmail.com>
2023-09-20 08:37:17 +0200

    Añadidos ejercicios

---
+++ CADENAS/cadenas.py
@@ -0,0 +1,23 @@
+'''Todos los ejercicios realizados y corregidos estaran en comentario
+
+
+fuente: https://aprendeconalf.es/docencia/python/ejercicios/cadenas/
+
+Ejercicio 1 
+nombre = input("Nombre: ")
+n = input("Numero entero: ")
+print((nombre + "\n") * int(n))
+
+Ejercicio 2
+nombre = input("Nombre completo: ")
+
+print(nombre.lower())
+print(nombre.upper())
+print(nombre.title())
+
+Ejercicio 3
+nombre = input("Nombre completo: ")
+print(nombre.upper(), "tiene", str(len(nombre)), "letras")
+
+Ejercicio 4
+'''
