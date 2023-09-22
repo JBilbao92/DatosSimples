e9dbe40b1fe615d5f9b72a1bf04b4e0238c9603e
Javier.Bilbao <jbilbao92@gmail.com>
2023-09-22 09:33:20 +0200

    Update ListasyTuplas.py

--- LISTAS Y TUPLAS/ListasyTuplas.py
+++ LISTAS Y TUPLAS/ListasyTuplas.py
@@ -7,4 +7,18 @@ Ejercicio 1
 asignaturas = ["Mate", "Fisica", "Quimica", "Historia", "Lengua"]
 print(asignaturas)
 
-Ejercicio2'''
+Ejercicio2
+asignaturas = ["Mate", "Fisica", "Quimica", "Historia", "Lengua"]
+for asignatura in asignaturas:
+    print("Estudio " + asignatura)
+
+Ejercicio 3
+asignaturas = ["Mate", "Fisica", "Quimica", "Historia", "Lengua"]
+notas = []
+for asignatura in asignaturas:
+    nota = input("Nota en " + asignatura + ":")
+    notas.append(nota)
+for i in range(len(asignaturas)):
+    print("En " + asignaturas[i] + " has sacado " + notas[i])
+
+Ejercicio 4'''
