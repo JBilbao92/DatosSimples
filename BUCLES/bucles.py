0fb70bfde697d647f07c28225ddd64e99818b184
Javier.Bilbao <jbilbao92@gmail.com>
2023-09-22 08:17:56 +0200

    Update bucles.py

--- BUCLES/bucles.py
+++ BUCLES/bucles.py
@@ -2,4 +2,26 @@
 
 
 fuente: https://aprendeconalf.es/docencia/python/ejercicios/bucles/
-'''
+
+
+Ejercicio 1
+palabra = input("Introduce una palabra: ")
+for i in range(10):
+    print(palabra)
+
+Ejercicio2
+años = int(input("Introduce tu edad: "))
+for i in range(años):
+    print("Has cumplido " + str(i+1) + " años")
+
+Ejercicio 3
+numero = int(input("Introduzca un numero entero positivo: "))
+for i in range(1, numero+1, 2):
+    print(i, end=", ")
+
+Ejercicio 4
+numero = int(input("Introduzca un numero entero positivo: "))
+for i in range(numero, -1, -1):
+    print(i, end=", ")
+
+Ejercicio 5'''
