0b2ef133199bb914abdfd2c7c7be705871672a52
Javier.Bilbao <jbilbao92@gmail.com>
2023-09-27 08:47:30 +0200

    Update ListasyTuplas.py

--- LISTAS Y TUPLAS/ListasyTuplas.py
+++ LISTAS Y TUPLAS/ListasyTuplas.py
@@ -28,4 +28,54 @@ for i in range (6):
 numero.sort()
 print("Los números ganadores son " + str(numero))
 
-Ejercicio 5'''
+Ejercicio 5
+numeros = [1,2,3,4,5,6,7,8,9,10]
+for i in range(1,11):
+    print(numeros[-i], end=", ")
+
+Ejercicio 6
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+aprobada = []
+for asignatura in asignaturas:
+    puntos = float(input("Nota en " + asignatura + ":"))
+    if puntos >= 5:
+        aprobada.append(asignatura)
+for asignatura in aprobada:
+    asignaturas.remove(asignatura)
+print("Tienes que repetir " + str(asignaturas))
+
+Ejercicio 7
+import string
+string.ascii_lowercase
+alfabeto = list(string.ascii_lowercase)
+for i in range(len(alfabeto), 1, -1):
+    if i % 3 == 0:
+        alfabeto.pop(i-1)
+print(alfabeto)
+
+Ejercicio 8
+palabra = input("Introduzca una palabra: ")
+palabra_reves = palabra
+palabra = list(palabra)
+palabra_reves = list(palabra_reves)
+palabra_reves.reverse()
+if palabra == palabra_reves:
+    print ("Palindromo")
+else:
+    print ("No es un palindromo")
+
+Ejercicio 10
+precios = [50, 75, 46, 22, 80, 65, 8]
+min = max = precios[0]
+for precio in precios:
+    if precio < min:
+        min = precio
+    elif precio > max:
+        max = precio
+print("Precio mínimo: " + str(min))
+print("Precio máximo: " + str(max))
+
+Ejercicio 11'''
+vector1 = (1, 2, 3)
+vector2 = (-1, 0, 2)
+producto = 0
